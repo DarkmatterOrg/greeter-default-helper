386e45d95b4aa411e2f6a8826d14c9ec2c6a17bc
Jerry Starke <42114389+JerrySM64@users.noreply.github.com>
2025-02-21 22:12:32 +0100

    Initial commit. Hello world!

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use std::{path::Path, process::Command};
+use std::fs;
+
+fn get_image_type() -> String {
+    
+    let image_type_file: Option<&Path>;
+
+    if Path::new("/usr/share/horizon").exists() {
+        image_type_file = Some(Path::new("/usr/share/horizon/image_type"));
+    } else if Path::new("/usr/share/nova/image_type").exists() {
+        image_type_file = Some(Path::new("/usr/share/nova/image_type"));
+    } else if Path::new("/usr/share/umbra/image_type").exists() {
+        image_type_file = Some(Path::new("/usr/share/umbra/image_type"));
+    } else {
+        // Return an error if no image type file is found
+        panic!("\x1b[1;31mERROR:\x1b[0m No image type file found!");
+    }
+
+    let image = fs::read_to_string(image_type_file.unwrap()).expect("Failed to get image type");
+
+    let image_name = image.trim().to_string();
+
+    return image_name
+}
+
+fn systemd_service(service_name: &str) {
+    let _set_graphical = Command::new("systemctl") 
+        .args(["set-default", "graphical.target"])
+        .status()
+        .expect("An error occured. Guess, I'm gonna have a panic attack now! :(");
+        
+    let _disable_service = Command::new("systemctl")
+        .args(["disable", service_name])
+        .status()
+        .expect("An error occured. Guess, I'm gonna have a panic attack now! :(");
+            
+    let _reboot = Command::new("systemctl")
+        .args(["reboot"])
+        .status()
+        .expect("An error occured. Guess, I'm gonna have a panic attack now! :(");
+}
+
+fn main() {
+    let image_type = get_image_type();
+  
+    if image_type.contains("nova") {
+        systemd_service("nova-first-boot.service");
+    } else if image_type == "plasma" {
+        systemd_service("umbra-first-boot.service");
+    }
+}
